d3bbc616c2e3b7f7a68afd3570bad70cf8b1549d
Faber Vergara <fabervergara@gmail.com>
2019-09-30 08:43:52 -0500

    remove example content and add google PDF viewer

--- pdf-widget.php
+++ pdf-widget.php
@@ -12,143 +12,85 @@ License: GPL2
 // The widget class
 class PDF_Widget extends WP_Widget {
 
-	// Main constructor
-	public function __construct() {
-		parent::__construct(
-			'my_custom_widget',
-			__( 'My Custom Widget', 'text_domain' ),
-			array(
-				'customize_selective_refresh' => true,
-			)
-		);
-	}
+  // Main constructor
+  public function __construct() {
+    parent::__construct(
+      'my_custom_widget',
+      __( 'PDF Reader', 'text_domain' ),
+      array(
+        'customize_selective_refresh' => true,
+      )
+    );
+  }
 
-	// The widget form (for the backend )
+  // The widget form (for the backend )
 public function form( $instance ) {
 
-	// Set widget defaults
-	$defaults = array(
-		'title'    => '',
-		'text'     => '',
-		'textarea' => '',
-		'checkbox' => '',
-		'select'   => '',
-	);
+  // Set widget defaults
+  $defaults = array(
+    'title'    => '',
+    'text'     => ''
+  );
 
-	// Parse current settings with defaults
-	extract( wp_parse_args( ( array ) $instance, $defaults ) ); ?>
+  // Parse current settings with defaults
+  extract( wp_parse_args( ( array ) $instance, $defaults ) ); ?>
 
-	<?php // Widget Title ?>
-	<p>
-		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Widget Title', 'text_domain' ); ?></label>
-		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-	</p>
+  <?php // Widget Title ?>
+  <p>
+    <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Title', 'text_domain' ); ?></label>
+    <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+  </p>
 
-	<?php // Text Field ?>
-	<p>
-		<label for="<?php echo esc_attr( $this->get_field_id( 'text' ) ); ?>"><?php _e( 'Text:', 'text_domain' ); ?></label>
-		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'text' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'text' ) ); ?>" type="text" value="<?php echo esc_attr( $text ); ?>" />
-	</p>
+  <?php // Text Field ?>
+  <p>
+    <label for="<?php echo esc_attr( $this->get_field_id( 'text' ) ); ?>"><?php _e( 'URL:', 'text_domain' ); ?></label>
+    <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'text' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'text' ) ); ?>" type="text" value="<?php echo esc_attr( $text ); ?>" />
+  </p>
+  <?php }
 
-	<?php // Textarea Field ?>
-	<p>
-		<label for="<?php echo esc_attr( $this->get_field_id( 'textarea' ) ); ?>"><?php _e( 'Textarea:', 'text_domain' ); ?></label>
-		<textarea class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'textarea' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'textarea' ) ); ?>"><?php echo wp_kses_post( $textarea ); ?></textarea>
-	</p>
+  // Update widget settings
+  public function update( $new_instance, $old_instance ) {
+    $instance = $old_instance;
+    $instance['title']    = isset( $new_instance['title'] ) ? wp_strip_all_tags( $new_instance['title'] ) : '';
+    $instance['text']     = isset( $new_instance['text'] ) ? wp_strip_all_tags( $new_instance['text'] ) : '';
 
-	<?php // Checkbox ?>
-	<p>
-		<input id="<?php echo esc_attr( $this->get_field_id( 'checkbox' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'checkbox' ) ); ?>" type="checkbox" value="1" <?php checked( '1', $checkbox ); ?> />
-		<label for="<?php echo esc_attr( $this->get_field_id( 'checkbox' ) ); ?>"><?php _e( 'Checkbox', 'text_domain' ); ?></label>
-	</p>
+    return $instance;
+  }
 
-	<?php // Dropdown ?>
-		<p>
-			<label for="<?php echo $this->get_field_id( 'select' ); ?>"><?php _e( 'Select', 'text_domain' ); ?></label>
-			<select name="<?php echo $this->get_field_name( 'select' ); ?>" id="<?php echo $this->get_field_id( 'select' ); ?>" class="widefat">
-			<?php
-			// Your options array
-			$options = array(
-				''        => __( 'Select', 'text_domain' ),
-				'option_1' => __( 'Option 1', 'text_domain' ),
-				'option_2' => __( 'Option 2', 'text_domain' ),
-				'option_3' => __( 'Option 3', 'text_domain' ),
-			);
+  // Display the widget
+  public function widget( $args, $instance ) {
 
-			// Loop through options and add each one to the select dropdown
-			foreach ( $options as $key => $name ) {
-				echo '<option value="' . esc_attr( $key ) . '" id="' . esc_attr( $key ) . '" '. selected( $select, $key, false ) . '>'. $name . '</option>';
+    extract( $args );
 
-			} ?>
-			</select>
-		</p>
-	<?php }
+    // Check the widget options
+    $title    = isset( $instance['title'] ) ? apply_filters( 'widget_title', $instance['title'] ) : '';
+    $text     = isset( $instance['text'] ) ? $instance['text'] : '';
 
-	// Update widget settings
-	public function update( $new_instance, $old_instance ) {
-		$instance = $old_instance;
-		$instance['title']    = isset( $new_instance['title'] ) ? wp_strip_all_tags( $new_instance['title'] ) : '';
-		$instance['text']     = isset( $new_instance['text'] ) ? wp_strip_all_tags( $new_instance['text'] ) : '';
-		$instance['textarea'] = isset( $new_instance['textarea'] ) ? wp_kses_post( $new_instance['textarea'] ) : '';
-		$instance['checkbox'] = isset( $new_instance['checkbox'] ) ? 1 : false;
-		$instance['select']   = isset( $new_instance['select'] ) ? wp_strip_all_tags( $new_instance['select'] ) : '';
-		return $instance;
-	}
+    // WordPress core before_widget hook (always include )
+    echo $before_widget;
 
-	// Display the widget
-	public function widget( $args, $instance ) {
+    // Display the widget
+    echo '<div class="widget-text wp_widget_plugin_box">';
 
-		extract( $args );
+      // Display widget title if defined
+      if ( $title ) {
+        echo $before_title . $title . $after_title;
+      }
 
-		// Check the widget options
-		$title    = isset( $instance['title'] ) ? apply_filters( 'widget_title', $instance['title'] ) : '';
-		$text     = isset( $instance['text'] ) ? $instance['text'] : '';
-		$textarea = isset( $instance['textarea'] ) ?$instance['textarea'] : '';
-		$select   = isset( $instance['select'] ) ? $instance['select'] : '';
-		$checkbox = ! empty( $instance['checkbox'] ) ? $instance['checkbox'] : false;
+      // Display text field
+      if ( $text ) {
+        echo '<iframe src="https://docs.google.com/viewer?url=' . $text . '&embedded=true" style="width:600px; height:350px;" frameborder="0"></iframe>';
+      }
 
-		// WordPress core before_widget hook (always include )
-		echo $before_widget;
+    // WordPress core after_widget hook (always include )
+    echo $after_widget;
 
-		// Display the widget
-		echo '<div class="widget-text wp_widget_plugin_box">';
-
-			// Display widget title if defined
-			if ( $title ) {
-				echo $before_title . $title . $after_title;
-			}
-
-			// Display text field
-			if ( $text ) {
-				echo '<p>' . $text . '</p>';
-			}
-
-			// Display textarea field
-			if ( $textarea ) {
-				echo '<p>' . $textarea . '</p>';
-			}
-
-			// Display select field
-			if ( $select ) {
-				echo '<p>' . $select . '</p>';
-			}
-
-			// Display something if checkbox is true
-			if ( $checkbox ) {
-				echo '<p>Something awesome</p>';
-			}
-
-		echo '</div>';
-
-		// WordPress core after_widget hook (always include )
-		echo $after_widget;
-
-	}
+  }
 
 }
 
 // Register the widget
 function my_register_custom_widget() {
-	register_widget( 'PDF_Widget' );
+  register_widget( 'PDF_Widget' );
 }
 add_action( 'widgets_init', 'my_register_custom_widget' );
